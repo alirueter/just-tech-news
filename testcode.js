6d73d5cf3f1f027ff30c3425d61596f2d7fedbc8
alirueter <rueter.alison@gmail.com>
2020-08-24 19:58:34 -0500

    working

---
+++ testcode.js
@@ -0,0 +1,4 @@
+// INSERT INTO user (username, email, password) VALUES ('testuser', 'test@test.com', 'test123');
+// INSERT INTO post (title, post_url, user_id, created_at, updated_at) VALUES ('google', 'google.com', '1', '2020-08-24', '2020-08-24');
+// INSERT INTO comment (comment_text, user_id, post_id, created_at, updated_at) VALUES ('comment', 1, 1, '2020-08-24', '2020-08-24');
+// INSERT INTO vote (user_id, post_id) VALUES (1, 1);
